9493f40fff9ccbb3c03a1f85ddfce25add1e3d22
Douglas_HdezT <00004516@uca.edu.sv>
2018-06-13 13:50:30 -0600

    Solucion de bug de la API

--- app/src/main/java/com/debugps/gamesnews/api/deserilizers/FavGSONDeserializer.java
+++ app/src/main/java/com/debugps/gamesnews/api/deserilizers/FavGSONDeserializer.java
@@ -10,18 +10,16 @@ import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FavGSONDeserializer implements JsonDeserializer<FavArrayListDataApi> {
     @Override
     public FavArrayListDataApi deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         final JsonObject jsonObject = json.getAsJsonObject();
 
-        final NewDataAPI[] favorites_news_objects = context.deserialize(jsonObject.get("favoriteNews"), NewDataAPI[].class);
-        final ArrayList<String> favorites_news_id = new ArrayList<>();
+        final String[] favorites_news_objects = context.deserialize(jsonObject.get("favoriteNews"), String[].class);
 
-        for (NewDataAPI favorites_news_object : favorites_news_objects) {
-            favorites_news_id.add(favorites_news_object.getId());
-        }
+        final ArrayList<String> favorites_news_id = new ArrayList<>(Arrays.asList(favorites_news_objects));
 
         return new FavArrayListDataApi(favorites_news_id);
     }
